3d19226a28b57ca05d26750e2548172391418e04
syd19 <syd1990nsu@gmail.com>
2016-11-10 23:17:51 +0600

    init commit

---
+++ app/src/main/java/com/smalam/pseudozero/pbpush/MainActivity.java
@@ -0,0 +1,13 @@
+package com.smalam.pseudozero.pbpush;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
