8cb15c1ef29e7db4d3adc75ca5cb5d8857d0111a
Sayed Mahmudul Alam <smalam119@gmail.com>
2017-05-26 13:04:10 +0600

    config changes

--- app/src/main/java/com/smalam/pseudozero/pbpush/Config.java
+++ app/src/main/java/com/smalam/pseudozero/pbpush/Config.java
@@ -6,10 +6,10 @@ package com.smalam.pseudozero.pbpush;
 
 public class Config {
 
-
-    public static final String SENDER_ID = "930554319484";
-    public static String PUB_KEY = "pub-c-9039ba87-013a-4a24-95dc-1c4104380dbf";
-    public static String SUB_KEY = "sub-c-e050bb00-ad64-11e6-b6b9-0619f8945a4f";
-    public static String CHANNEL_NAME = "testChannel";
+    //replace by obtained keys
+    public static final String SENDER_ID = "xxxxxxxxxxxx";
+    public static String PUB_KEY = "pub-c-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+    public static String SUB_KEY = "sub-c-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+    public static String CHANNEL_NAME = "channelName";
     public static final String TOKEN = "token";
 }
